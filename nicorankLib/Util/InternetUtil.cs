6a78c30000ef98aeb1ba4e0076911a6d6949f5eb
２代目 <126141697+n2daime@users.noreply.github.com>
2025-04-07 19:12:07 +0900

    ファイルダウンロード機能を追加

--- nicorankLib/Util/InternetUtil.cs
+++ nicorankLib/Util/InternetUtil.cs
@@ -56,32 +56,88 @@ namespace nicorankLib.Util
 
                         switch (ex.Status)
                         {
-                            case WebExceptionStatus.ProtocolError:          
+                            case WebExceptionStatus.ProtocolError:
                             case WebExceptionStatus.Timeout:
 
                                 Thread.Sleep(1000);
-                                continue; 
+                                continue;
                         }
+                    }
+                    catch (System.IO.IOException ex)
+                    {
+                        Thread.Sleep(1000);
+                        continue;
                     }
                     break;
                 }
             }
-
-
-
-
-            //HttpWebRequest req = (HttpWebRequest)System.Net.WebRequest.Create(url);
-            //req.UserAgent = "WeeklyNicoranProgram";
-            //WebResponse res = req.GetResponse();
-            //stream = res.GetResponseStream();
             if (stream == null)
             {
                 return false;
             }
-
-
-
             return true;
         }
+        /// <summary>
+        /// インターネット上のファイルを保存する
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="readText"></param>
+        /// <param name="encode"></param>
+        /// <returns></returns>
+        public static bool FileDownLoad(string url, string fileName)
+        {
+            Stream stream = null;
+            using (var web = new WebClient())
+            {
+                web.Headers.Add("User-Agent", "WeeklyNicoranProgram");
+                for (int count = 0; count < 20; count++)
+                {
+                    try
+                    {
+                        using (stream = web.OpenRead(url))
+                        {
+                            using (var file = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write))
+                            {
+                                int read;
+                                byte[] buffer = new byte[1024];
+                                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                                {
+                                    file.Write(buffer, 0, read);
+                                }
+                            }
+                        }
+                    }
+                    catch (WebException ex)
+                    {
+                        if (ex.Status == WebExceptionStatus.ProtocolError && ex.Response.ContentType == "application/xml")
+                        {
+                            //真面目にやるのであれば中身をちゃんと解析するべきだが、AccessDeniedと見直して、再試行を抜ける
+                            break;
+                        }
+
+                        switch (ex.Status)
+                        {
+                            case WebExceptionStatus.ProtocolError:
+                            case WebExceptionStatus.Timeout:
+
+                                Thread.Sleep(1000);
+                                continue;
+                        }
+                    }
+                    catch (System.IO.IOException ex)
+                    {
+                        Thread.Sleep(1000);
+                        continue;
+                    }
+                    break;
+                }
+            }
+            if (stream == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
     }
 }
